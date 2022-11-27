8db5f9d61f53bcade7bfb124548bee218fb04955
JinyoungCheon <ssconvergence2@gmail.com>
2022-11-28 02:17:16 +0900

    Datamining SVM class

--- Datamining_FINAL.R
+++ Datamining_FINAL.R
@@ -129,3 +129,111 @@ summary(boost.boston)
 yhat.boost <- predict(boost.boston, Boston[test,], n.trees=5000)
 mean((yhat.boost-y)^2) # RF보다 좋은 성능
 
+# 시뮬레이션 데이터 생성
+# generate training data 
+set.seed(1)
+x <- matrix(rnorm(20*2), ncol = 2)
+y <- c(rep(-1, 10), rep(1,10))
+
+x[y==1, ] <- x[y==1] + 1 
+plot(x, col = (3-y), pch = 16) # y=1 : 2='red', y=-1 : 4='blue
+
+dat <- data.frame(x=x, y=as.factor(y))
+head(dat)
+
+# generate test data
+xtest <- matrix(rnorm(20*2), ncol=2)
+ytest <- sample(c(-1, 1), 20, rep=TRUE)
+xtest[ytest==1,] <- xtest[ytest==1,] + 1
+testdat <- data.frame(x=xtest, y=as.factor(ytest))
+
+
+# Support Vector Machine
+install.packages("e1071")
+library(e1071)
+
+svmfit <- svm(y ~ ., data = dat, kernel = 'linear', cost=10, scale=F)
+plot(svmfit, dat)
+svmfit
+
+svmfit$index
+
+# 10-fold CV
+set.seed(1)
+tune.out <- tune(svm, y~., data=dat, kernel = 'linear', 
+                 ranges=list(cost=c(0.001, 0.01, 0.1, 1, 5, 10, 100)))
+
+summary(tune.out) 
+bestmod <-tune.out$best.model
+bestmod
+
+ypred <- predict(bestmod, newdata = testdat)
+table(ypred, testdat$y)
+
+# Support Vector Machine (non-linear)
+# generate data
+set.seed(1)
+x <- matrix(rnorm(200*2), ncol = 2)
+x[1:100, ] <- x [1:100,] + 2
+x[101:150, ] <- x[101:150, ] -2 
+y <- c(rep(1,150), rep(2,50))
+dat <- data.frame(x=x, y=as.factor(y))
+head(dat)
+plot(x, col=y+1, pch =16) # y=1 : col=2(red), y=2 : col=3(green)
+
+# fit SVM with radial kernel
+train <- sort(sample(200,100))
+test <- setdiff(1:200, train)
+
+svmfit <- svm(y~., data=dat[train, ], kernel ='radial',
+              gamma=1, cost = 1)
+summary(svmfit)
+plot(svmfit, dat[train,])
+
+# 10-fold CV
+set.seed(1)
+tune.out <- tune(svm, y~., data=dat[train, ], kernel ='radial',
+                 ranges=list(cost=c(0.1, 1, 10, 100, 1000), gamma = c(0.5, 1,2,3,4)))
+tune.out
+summary(tune.out)
+
+testy <- dat[test, ]$y
+predy <- predict(tune.out$best.model, newx = dat[test, ])
+
+table(testy, predy)
+
+## (3) ROC Curves
+library(ROCR)
+rocplot=function(pred, truth, ...){
+  predob = prediction(pred, truth)
+  perf = performance(predob, "tpr", "fpr")
+  plot(perf,...)
+}
+
+svmfit.opt=svm(y~., data=dat[train,], kernel="radial",gamma=2, cost=1,decision.values=T) # decision.values=T로 설정하면 f(x)값 계산
+fitted=attributes(predict(svmfit.opt,dat[train,],decision.values=TRUE))$decision.values
+par(mfrow=c(1,2))
+rocplot(fitted,dat[train,"y"],main="Training Data")
+svmfit.flex=svm(y~., data=dat[train,], kernel="radial",gamma=50, cost=1, decision.values=T)
+fitted=attributes(predict(svmfit.flex,dat[train,],decision.values=T))$decision.values
+rocplot(fitted,dat[train,"y"],add=T,col="red")
+fitted=attributes(predict(svmfit.opt,dat[-train,],decision.values=T))$decision.values
+rocplot(fitted,dat[-train,"y"],main="Test Data")
+fitted=attributes(predict(svmfit.flex,dat[-train,],decision.values=T))$decision.values
+rocplot(fitted,dat[-train,"y"],add=T,col="red")
+
+## (4) SVM with Multiple Classes
+
+set.seed(1)
+x=rbind(x, matrix(rnorm(50*2), ncol=2))
+y=c(y, rep(0,50))
+x[y==0,2]=x[y==0,2]+2
+dat=data.frame(x=x, y=as.factor(y))
+
+par(mfrow=c(1,1))
+plot(x,col=(y+1), pch=16)
+
+svmfit=svm(y~., data=dat, 
+           kernel="radial", 
+           cost=10, gamma=1)
+plot(svmfit, dat)
